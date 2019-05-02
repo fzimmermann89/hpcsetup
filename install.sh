4bd4790570f252622d1821fc617f7cf1b504f525
Felix Zimmermann <fzimmermann89@gmail.com>
2019-05-02 13:15:13 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DEST=$HOME/local
+
+TEXTPLACEHOLDER="/home/gorkhover/local/"
+TEXTNEW="$DEST"
+while read -r file ; do
+    echo "Processing $file"
+    sed -i -e 's#$TEXTPLACEHOLDER#$TEXTNEW#g' $file
+done < <(grep -al "$TEXTPLACEHOLDER" -r local/ )
+
+BINPLACEHOLDER="/home/gorkhover/////local/"
+BINNEW="$DEST//////////////////////////////"
+BINNEW=${BINNEW:0:26}
+while read -r file ; do
+    echo "Processing $file"
+    sed -i -e 's#$BINPLACEHOLDER#$BINNEW#g' $file
+done < <(grep -al "$BINPLACEHOLDER" -r local/ )
+
+cp -rP local/* $DEST
+cp -P .* $HOME
+cp -rPp .oh-my-zsh $HOME
